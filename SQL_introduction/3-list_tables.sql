f8e27fef9cc1b1a15886309c701876aab15dec64
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-14 21:37:47 +0200

    Task3 commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,7 @@
 -- a script that lists all the tables of a database in your MySQL server
-SET @dbname = hbtn_0c_0;
+-- SET @dbname = hbtn_0c_0;
+db_name="$1"
 -- first select the database 
-USE @dbname;
+USE $db_name;
 -- list tables
-
 SHOW tables;
