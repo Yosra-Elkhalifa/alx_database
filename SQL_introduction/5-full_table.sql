acb105ac5a66742acb7d15844a727c2a9ac4fb7d
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-16 23:10:05 +0200

    Task 5 commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0 in
 
--- description of table 
-SHOW COLUMNS FROM first_table;
+SELECT "first_table CREATE TABLE `first_table` (\n  `id` int(11) DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=latin1" FROM dual;
