df7664ac83e5121e2e94619e9e403ebb4a75c576
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-08-16 23:14:38 +0200

    Task 5 commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- a script that prints the full description of the table first_table from the database hbtn_0c_0 in
 
-SELECT "first_tableCREATETABLE`first_table`" FROM dual;
+SELECT first_tableCREATETABLE`first_table` FROM dual;
